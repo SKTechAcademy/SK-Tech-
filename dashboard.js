cc3cdb109e1899c93828a1989f9f84be901bd9d4
SK Tech <support@mysktech.com>
2026-06-20 14:10:25 +0530

    Refactor loadData function to use SCRIPT_URL

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,14 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbzzML8wx7lF_0caPkVZVsLgpHvPX1brwcgWPW8Pg8t4EwYzUQHsMmB_R8AJdDSjA3t1RA/exec";
+const SCRIPT_URL =
+"https://script.google.com/macros/s/AKfycbzzML8wx7lF_0caPkVZVsLgpHvPX1brwcgWPW8Pg8t4EwYzUQHsMmB_R8AJdDSjA3t1RA/exec?callback=loadData";
 
-async function loadData(){
+const script = document.createElement("script");
+script.src = SCRIPT_URL;
+document.body.appendChild(script);
 
-const response = await fetch(API_URL);
+function loadData(data){
 
-const data = await response.json();
+```
+console.log(data);
 
 document.getElementById("totalCount").innerText =
 data.length;
@@ -13,28 +16,24 @@ data.length;
 let table =
 document.getElementById("tableBody");
 
-table.innerHTML="";
+table.innerHTML = "";
 
-data.forEach(item=>{
+data.forEach(item => {
 
-table.innerHTML += `
-<tr>
-<td>${item["Sk Tech Register ID"]}</td>
-<td>${item["Full Name"]}</td>
-<td>${item["Round"]}</td>
-<td>${item["Technologies Required"]}</td>
-<td>${item["Interview Company"]}</td>
-<td>${item["Interview Date"]}</td>
-<td>${item["Interview Time (From)"]}</td>
-<td>${item["Interview Time (To)"]}</td>
-<td>${item["Batch"]}</td>
-</tr>
-`;
-
+    table.innerHTML += `
+    <tr>
+        <td>${item["Sk Tech Register ID"]}</td>
+        <td>${item["Full Name"]}</td>
+        <td>${item["Round"]}</td>
+        <td>${item["Technologies Required"]}</td>
+        <td>${item["Interview Company"]}</td>
+        <td>${item["Interview Date"]}</td>
+        <td>${item["Interview Time (From)  or  If Time Not confirmed plz select 00:00 like Assessment"]}</td>
+        <td>${item["Interview Time (To) or  If Time Not confirmed plz select 00:00 like Assessment"]}</td>
+        <td>${item["Batch"]}</td>
+    </tr>
+    `;
 });
+```
 
 }
-
-loadData();
-
-setInterval(loadData,30000);
