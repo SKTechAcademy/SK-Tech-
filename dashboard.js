86428775aa3f9f91817607f56c0e51a4b934cb00
SK Tech <support@mysktech.com>
2026-06-20 13:59:21 +0530

    Refactor loadInterviews to loadData and update table

--- dashboard.js
+++ dashboard.js
@@ -1,31 +1,40 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbzzML8wx7lF_0caPkVZVsLgpHvPX1brwcgWPW8Pg8t4EwYzUQHsMmB_R8AJdDSjA3t1RA/exec";
 
-async function loadInterviews() {
+async function loadData(){
 
-    const response = await fetch(API_URL);
-    const data = await response.json();
+const response = await fetch(API_URL);
 
-    const table =
-    document.getElementById("tableBody");
+const data = await response.json();
 
-    table.innerHTML = "";
+document.getElementById("totalCount").innerText =
+data.length;
 
-    data.forEach(item => {
+let table =
+document.getElementById("tableBody");
 
-        table.innerHTML += `
-        <tr>
-            <td>${item["Sk Tech Register ID"]}</td>
-            <td>${item["Full Name"]}</td>
-            <td>${item["Technologies Required"]}</td>
-            <td>${item["Interview Company"]}</td>
-            <td>${item["Interview Date"]}</td>
-            <td>${item["Interview Time (From)"]}</td>
-            <td>${item["Interview Time (To)"]}</td>
-        </tr>`;
-    });
+table.innerHTML="";
+
+data.forEach(item=>{
+
+table.innerHTML += `
+<tr>
+<td>${item["Sk Tech Register ID"]}</td>
+<td>${item["Full Name"]}</td>
+<td>${item["Round"]}</td>
+<td>${item["Technologies Required"]}</td>
+<td>${item["Interview Company"]}</td>
+<td>${item["Interview Date"]}</td>
+<td>${item["Interview Time (From)"]}</td>
+<td>${item["Interview Time (To)"]}</td>
+<td>${item["Batch"]}</td>
+</tr>
+`;
+
+});
+
 }
 
-loadInterviews();
+loadData();
 
-setInterval(loadInterviews,30000);
+setInterval(loadData,30000);
