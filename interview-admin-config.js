44acd076d4c22833212a2f660f970144692c8a35
SK Tech <support@mysktech.com>
2026-08-16 14:17:30 +0530

    Configure Google admin sign-in client

--- interview-admin-config.js
+++ interview-admin-config.js
@@ -1,7 +1,7 @@
 window.SKTECH_INTERVIEW_ADMIN_CONFIG = {
   // Create a Google OAuth 2.0 Web Client ID and add your live/local origins.
   // A Client ID is safe to publish; authorization is enforced again by Apps Script.
-  googleClientId: "REPLACE_WITH_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com",
+  googleClientId: "299606117243-adnn58ov2jrb4eodv1m8d2dpdu3edsrb.apps.googleusercontent.com",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbwxpMoYA7gmul9iMk9eA2Cae07sxynCp6Ff73BhXFAdJoOMBmNzZP2-5ck2qRyqjm7W/exec",
   refreshMilliseconds: 30000
 };
